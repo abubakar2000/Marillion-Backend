4b4329a66d8fe2c8360b41bf1d7bd19ae62ea830
abubakar2000 <abubakar.mughal2ooo@icloud.com>
2023-09-05 14:19:54 +0500

    feat: unique users

--- dashboard/src/environment.ts
+++ dashboard/src/environment.ts
@@ -1,3 +1,3 @@
 // export const API_URL = "http://52.56.204.249:1337/api";
-// export const API_URL = "https://apps.marillionex.com:1337/api";
-export const API_URL = "http://127.0.0.1:1337/api";
+export const API_URL = "https://apps.marillionex.com:1337/api";
+// export const API_URL = "http://127.0.0.1:1337/api";
